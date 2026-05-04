0941dc59beb82166be928972117f37b99b081040
omarinho77 <omaraniki15@gmail.com>
2026-05-05 00:56:52 +0200

    Update es06b.js

--- esercizi/es06b.js
+++ esercizi/es06b.js
@@ -1,8 +1,25 @@
-function inverti_cifre(numero) {
-    let negativo = numero < 0;
-    numero = Math.abs(numero);
+const readline = require("readline");
 
-    let invertito = parseInt(numero.toString().split('').reverse().join(''));
-
-    return negativo ? -invertito : invertito;
+function inverti_cifre(n) {
+    const segno = Math.sign(n);
+    const invertito = parseInt(Math.abs(n).toString().split("").reverse().join(""));
+    return segno * invertito;
 }
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question("Inserisci un numero intero: ", (input) => {
+    const numero = parseInt(input);
+
+    if (isNaN(numero)) {
+        console.log("Errore: devi inserire un numero valido.");
+    } else {
+        const risultato = inverti_cifre(numero);
+        console.log("Numero invertito:", risultato);
+    }
+
+    rl.close();
+});
